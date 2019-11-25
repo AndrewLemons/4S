53dfbd44347e2b4e453c32a4d0ce05928947eb9e
Thermacon <30552077+Thermacon@users.noreply.github.com>
2019-11-24 22:38:58 -0500

    Login System Beginings
    Basic layout created for the future login system.

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require('express');
-const app = express();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const fs = require('fs');
+const mysql = require('mysql');
+var  randomString = require("randomstring");
 
+//Express
+const app = express();
 app.use(express.static('public'));
-
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.set('trust proxy', true)
 
-var rawUserData = fs.readFileSync('users.json');
-var users = JSON.parse(rawUserData);
+//Database
+var db = mysql.createConnection({
+	host: "localhost",
+	user: "nodeServer",
+	password: "password1234",
+	database: "4S"
+});
 
-var schoolStatus = "Safe";
+db.connect(function(err) {
+	if (err) throw err;
+});
 
+//Requests
 app.get('/', (req, res) => {
 	res.redirect('/login')
 });
@@ -22,15 +35,36 @@ app.get('/login', (req, res) => {
 	res.render("login");
 });
 
+app.post('/login', (req, res) => {
+	db.query("SELECT * FROM users WHERE username = '"+ req.body.username +"'", function (err, users) {
+		if (users.length > 0) {
+			if (req.body.password == users[0].password) {
+				var newKey = randomString.generate(32);
+				console.log(users[0].userid);
+				db.query("UPDATE users SET `activeKey` = '"+newKey+"' WHERE (`userid` = '"+users[0].userid+"')", function (err, result) {
+					res.cookie("key", newKey);
+					res.redirect("/console");
+				});
+			} else {
+				res.status(403).send('Incorrect Password');
+			}
+		} else {
+			res.status(403).send('Incorrect Username');
+		}
+	});
+});
+
 app.get('/console', (req, res) => {
-	res.render("console", {status: schoolStatus});
+	res.render("console", {status: "Safe"});
 });
 
-app.get('/ip', (req, res) => {
+app.get('/info', (req, res) => {
 	var ip = req.ip;
-	res.render("ip", {ip: ip});
+	var cookies = JSON.stringify(req.cookies);
+	res.render("info", {ip: ip, cookies: cookies});
 });
 
+//Start listening
 const server = app.listen(80, () => {
 	console.log(`Express running → PORT ${server.address().port}`);
 });
